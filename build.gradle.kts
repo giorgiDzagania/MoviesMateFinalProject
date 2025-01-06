add6d64d7dbf43c768aa1ad0de6a050cc16c19a8
GFO <gia.focxverashvili@gmail.com>
2025-01-06 15:07:30 +0400

    add

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,5 +12,6 @@ buildscript {
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-    id("com.google.devtools.ksp") version "2.0.0-1.0.24" apply false // ksp
+    id("com.google.devtools.ksp") version "2.0.0-1.0.24" apply false
+    alias(libs.plugins.google.gms.google.services) apply false // ksp
 }
