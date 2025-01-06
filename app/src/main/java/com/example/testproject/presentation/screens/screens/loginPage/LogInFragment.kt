113db423b7af9e2a3d59768afdc121c9efb7b291
GFO <gia.focxverashvili@gmail.com>
2025-01-06 12:43:50 +0400

    new com

--- app/src/main/java/com/example/testproject/presentation/screens/screens/loginPage/LogInFragment.kt
+++ app/src/main/java/com/example/testproject/presentation/screens/screens/loginPage/LogInFragment.kt
@@ -5,19 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.testproject.databinding.FragmentLoginBinding
 
 class LogInFragment:Fragment() {
+    private lateinit var binding : FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
-
 }
